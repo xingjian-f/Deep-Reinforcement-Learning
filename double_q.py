1ab1bc589c8a46a7cc6a7be66e357c5f1b4bd13c
FEI Xing-jian <feixingjian@ipin.com>
2017-04-19 18:15:48 +0800

    more universal

--- double_q.py
+++ double_q.py
@@ -1,31 +1,32 @@
+from collections import deque
+import random
+import time
+import json
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np 
-import random
-import time
-import json
 import gym
 from gym import wrappers
 from keras.models import load_model
 
 
-def mlp():
+def mlp(state_size, action_size):
 	from keras.models import Sequential
 	from keras.layers import Dense, Flatten, Input
 
 	model = Sequential()
-	model.add(Dense(32, activation='relu', input_shape=(500,)))
+	model.add(Dense(32, activation='relu', input_shape=(state_size,)))
 	model.add(Dense(32, activation='relu'))
-	model.add(Dense(6))
+	model.add(Dense(action_size))
 	model.compile(loss='mse', optimizer='RMSprop', metrics=['mae'])
 	model.summary()
 
 	return model 
 
 
-def one_hot_encoder(n):
-	ret = np.zeros(500)
+def one_hot_encoder(n, vec_size):
+	ret = np.zeros(vec_size)
 	ret[n] = 1
 	return ret
 
@@ -66,56 +67,65 @@ def plot(rewards, q):
 	plt.savefig('models/index.jpg')
 
 
-def train():
-	from collections import deque
+def e_greedy(epsilon, env, observation, state_size, action_function):
+	if np.random.random() < epsilon:
+		action = env.action_space.sample()
+	else:
+		state = np.asarray([one_hot_encoder(observation, state_size)])
+		q_values = action_function.predict(state)[0]
+		# print q_values
+		action = np.argmax(q_values)
+	return action 
 
-	action_function = mlp()
-	estimate_function = mlp()
+
+def update_memory(replay_memory, max_replay_memory_size, observation, next_observation,
+	state_size, action, reward, done):
+	if len(replay_memory) > max_replay_memory_size:
+		replay_memory.popleft()
+	replay_memory.append((one_hot_encoder(observation, state_size), action, 
+		one_hot_encoder(next_observation, state_size), reward, 0 if done else 1))
+
+
+def train():
+	# game info
+	# game_name = 'Taxi-v2'
+	game_name = 'FrozenLake-v0'
+	env = gym.make(game_name)
+	state_size = env.observation_space.n 
+	action_size = env.action_space.n
+	print 'State size', state_size
+	print 'Action size', action_size
+
+	# initialize deep model
+	action_function = mlp(state_size, action_size)
+	estimate_function = mlp(state_size, action_size)
 	# action_function = load_model('models/actionQ_7800_-2.22')
 	# estimate_function = load_model('models/estimateQ_7800_-2.22')
-	real_q = json.loads(open('Taxi_q').readline())
-	env = gym.make("Taxi-v2")
+
+	# set the hyper-parameters 
 	episode_reward_tracker = []
 	episode_len_tracker = []
-	ave_q_error = []
-	max_q_error = []
 	replay_memory = deque()
 	max_replay_memory_size = 1000000
 	min_replay_memory_size = 100000
 	epsilon_start = 1
 	epsilon_end = 0.1
-	epsilon_decay_steps = 2000000
+	epsilon_decay_steps = 100000
 	epsilon_decay_val = (epsilon_start-epsilon_end) / epsilon_decay_steps
 	epsilon = epsilon_start
-	nb_episodes = 20000
+	nb_episodes = 200000
 	max_episode_steps = 200
 
 	for t in range(nb_episodes):
 		observation = env.reset()
-		# env.render()
 		whole_reward = 0
 		for step in range(max_episode_steps):
 			epsilon = max(epsilon_end, epsilon - epsilon_decay_val) 
-			if np.random.random() < epsilon:
-				action = env.action_space.sample()
-			else:
-				state = np.asarray([one_hot_encoder(observation)])
-				q_values = action_function.predict(state)[0]
-				standard_q = [real_q[str((observation, i))] for i in range(6)]
-				right_action = np.argmax(standard_q)
-				# print q_values
-				# print standard_q # taxi real Q
-				ave_q_error.append(sum([np.abs(q_values[i]-standard_q[i]) for i in range(6)]) / 6)
-				max_q_error.append(np.abs(q_values[right_action]-standard_q[right_action]))
-				action = np.argmax(q_values)
-			next_observation, reward, done, _ = env.step(action)
+			action = e_greedy(epsilon, env, observation, state_size, action_function) # select action by e-greedy
+			next_observation, reward, done, _ = env.step(action) # take action
 			whole_reward += reward
-			# env.render()
-			if len(replay_memory) > max_replay_memory_size:
-				replay_memory.popleft()
-			replay_memory.append((one_hot_encoder(observation), action, 
-				one_hot_encoder(next_observation), reward, 0 if done else 1))
-
+			update_memory(replay_memory, max_replay_memory_size, observation, next_observation,
+				state_size, action, reward, done)
 			if len(replay_memory) > min_replay_memory_size:
 				replay(replay_memory, action_function, estimate_function, step)
 				
@@ -123,8 +133,8 @@ def train():
 				# Save model
 				episode_reward_tracker.append(whole_reward)
 				episode_len_tracker.append(step+1)
-				print t, step, sum(ave_q_error[-200:]) / 200, sum(max_q_error[-200:]) / 200
-				if t % 100 == 0:
+				print t, step, epsilon, sum(episode_reward_tracker[-200:]) / 200, sum(episode_len_tracker[-200:]) / 200
+				if t % 1000 == 0 and len(replay_memory) > min_replay_memory_size:
 					plot(episode_reward_tracker, episode_len_tracker)
 					action_function.save('models/actionQ_%d_%.2lf' % (t, whole_reward))
 					estimate_function.save('models/estimateQ_%d_%.2lf' % (t, whole_reward))
@@ -134,10 +144,13 @@ def train():
 
 
 def play():
-	env = gym.make('Taxi-v2')
+	# game_name = 'Taxi-v2'
+	game_name = 'FrozenLake-v0'
+	env = gym.make(game_name)
+	state_size = env.observation_space.n 
 	action_function = load_model('models/actionQ_15300_-0.76')
 	estimate_function = load_model('models/estimateQ_15300_-0.76')
-	nb_episodes = 20000
+	nb_episodes = 100
 	max_episode_steps = 200
 	epsilon = 0.05
 
@@ -149,7 +162,7 @@ def play():
 			if np.random.random() < epsilon:
 				action = env.action_space.sample()
 			else:
-				state = np.asarray([one_hot_encoder(observation)])
+				state = np.asarray([one_hot_encoder(observation, state_size)])
 				q_values = action_function.predict(state)[0]
 				print q_values
 				action = np.argmax(q_values)
